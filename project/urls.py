a977642ac299b088c8624dc9abb3c4e42a9b4242
swathinathn <sn8958204@gmail.com>
2026-03-11 12:15:43 +0530

    update

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include   
+from django.urls import path
+from django.shortcuts import redirect
+from myapp import views
 
 urlpatterns = [
+    path('', lambda request: redirect('home')),
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('home/', views.home, name='home'),
 ]
